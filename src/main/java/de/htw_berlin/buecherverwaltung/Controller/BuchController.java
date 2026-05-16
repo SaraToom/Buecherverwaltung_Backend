c8abb7cc80f808bb9296c6e6f5c82e644e9d01a2
SaraToom <sara.toom@student.htw-berlin.de>
2026-05-16 15:01:39 +0200

    Backend vorbereiten

--- src/main/java/de/htw_berlin/buecherverwaltung/Controller/BuchController.java
+++ src/main/java/de/htw_berlin/buecherverwaltung/Controller/BuchController.java
@@ -3,10 +3,12 @@ package de.htw_berlin.buecherverwaltung.Controller;
 import de.htw_berlin.buecherverwaltung.Modell.Buch; 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class BuchController {
 
